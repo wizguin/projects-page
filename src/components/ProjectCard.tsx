1b7f3ebd95ec91f08ed26f4bf2e3ea5bbbbe1388
wizguin <16276645+wizguin@users.noreply.github.com>
2024-02-22 11:17:32 +0000

    Add ProjectCard badges

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,9 +1,10 @@
 interface Props {
     title: string,
-    description: string
+    description: string,
+    badges?: string[],
 }
 
-export default function ProjectCard({ title, description }: Props) {
+export default function ProjectCard({ title, description, badges = [] }: Props) {
     return (
         <section className="container px-4 pb-4">
             <div className="card">
@@ -12,6 +13,14 @@ export default function ProjectCard({ title, description }: Props) {
                     <div className="col-md-8">
                         <div className="card-body m-4">
                             <h5 className="card-title">{title}</h5>
+
+                            <div className="my-2">
+                                {
+                                    badges.map((text) => (
+                                        <span className="badge text-bg-secondary me-1">{text}</span>
+                                    ))
+                                }
+                            </div>
 
                             <p className="card-text">
                                 {description}
