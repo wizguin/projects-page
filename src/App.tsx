3b54bde023c363dc7a7451599fcc284d429559d3
wizguin <16276645+wizguin@users.noreply.github.com>
2024-02-22 11:16:26 +0000

    Add sample ProjectCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 
 import Header from './components/Header'
+import ProjectCard from './components/ProjectCard'
 
 function App() {
     return (
         <>
             <Header headerTitle='Portfolio'/>
+
+            <ProjectCard
+                title='Project'
+                description='
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                '
+            />
         </>
     )
 }
