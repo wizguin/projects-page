8d718bea91d612ff7e86c8c032d09091c88dc983
wizguin <16276645+wizguin@users.noreply.github.com>
2024-02-22 11:17:50 +0000

    Add sample badges

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 '
+                badges={ ['Node.js', 'React'] }
             />
         </>
     )
