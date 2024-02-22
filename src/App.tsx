04408597d23bf8b0e82ad3c4fcf73d2c21fa0849
wizguin <16276645+wizguin@users.noreply.github.com>
2024-02-22 14:09:15 +0000

    Add sample preview image

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
                 badges={ ['Node.js', 'React'] }
                 demoUrl='demo'
                 sourceUrl='source'
+                previewFile='preview.jpg'
             />
         </>
     )
