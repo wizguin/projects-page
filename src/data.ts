d6c0ef74a643159d28a76cdbf8b9af67383e2c94
wizguin <16276645+wizguin@users.noreply.github.com>
2024-03-11 14:47:03 +0000

    Add projects to data object

--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,27 @@
+import { Props as Project } from './components/ProjectCard'
+import PreviewTypes from './components/types/PreviewTypes'
+
 interface Data {
     headerTitle: string,
+    projects: Project[]
 }
 
 const data:Data = {
     headerTitle: 'Portfolio',
+    projects: [
+        {
+            title: 'Project',
+            description: `
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            `,
+            badges: ['Node.js', 'React'],
+            demoUrl: 'demo',
+            sourceUrl: 'source',
+            previewType: PreviewTypes.Image,
+            previewFile: 'preview.jpg'
+        }
+    ]
 }
 
 export default data
