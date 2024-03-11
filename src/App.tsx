546641539f9222b325fb0d6d4007343534377300
wizguin <16276645+wizguin@users.noreply.github.com>
2024-03-11 14:54:59 +0000

    Use projects data from data object

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,21 @@ import ProjectCard from './components/ProjectCard'
 
 
 function App() {
+    const projects = data.projects.map(project => (
+        <ProjectCard
+            title={project.title}
+            description={project.description}
+            badges={project.badges}
+            demoUrl={project.demoUrl}
+            sourceUrl={project.sourceUrl}
+            previewFile={project.previewFile}
+        />
+    ))
+
     return (
         <>
-            <Header headerTitle={data.headerTitle}/>
+            <Header headerTitle={data.headerTitle} />
+            {projects}
         </>
     )
 }
