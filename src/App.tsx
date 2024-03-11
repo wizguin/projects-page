c5c753a3402a62514f90e120543546137b4b038e
wizguin <16276645+wizguin@users.noreply.github.com>
2024-03-11 14:45:54 +0000

    Update app to use headerTitle from data object

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import './App.css'
 
+import data from './data'
 import Header from './components/Header'
 import ProjectCard from './components/ProjectCard'
 
-import PreviewTypes from './components/types/PreviewTypes'
 
 function App() {
     return (
         <>
-            <Header headerTitle='Portfolio'/>
-
-            <ProjectCard
-                title='Project'
-                description='
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                '
-                badges={ ['Node.js', 'React'] }
-                demoUrl='demo'
-                sourceUrl='source'
-                previewFile='preview.jpg'
-            />
+            <Header headerTitle={data.headerTitle}/>
         </>
     )
 }
