5c2c18f3dbd23eb3bf6840d98c9df6d5b8d70057
wizguin <16276645+wizguin@users.noreply.github.com>
2024-03-20 15:37:57 +0000

    Add basic video playback

--- src/components/media/Video.tsx
+++ src/components/media/Video.tsx
@@ -1,14 +1,32 @@
+import { useState, useRef, useEffect, MutableRefObject } from 'react'
+
 interface Props {
     src: string
     className: string
 }
 
 export default function Video({ src, className }: Props) {
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    const ref: MutableRefObject<HTMLVideoElement | null> = useRef(null)
+
+    useEffect(() => {
+        if (isPlaying) {
+            ref.current?.play()
+        } else {
+            ref.current?.pause()
+        }
+    }, [isPlaying])
+
+    function togglePlayback() {
+        setIsPlaying(prev => !prev)
+    }
+
     return (
         <>
             <i className='play-button fa-solid fa-play position-absolute top-50 start-50 translate-middle'></i>
 
-            <video className={className} role='button' onClick={() => console.log('test')}>
+            <video ref={ref} className={className} role='button' onClick={togglePlayback}>
                 <source src={src} />
             </video>
         </>
