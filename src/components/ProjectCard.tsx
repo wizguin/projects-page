e49ed9f947049e62c1538f1b4bd220d64d3b4dbc
wizguin <16276645+wizguin@users.noreply.github.com>
2024-03-20 14:53:08 +0000

    Fix ProjectCard quote style

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -13,24 +13,24 @@ export interface Props {
 
 export default function ProjectCard({ title, description, badges, demoUrl, sourceUrl, previewType, previewFile }: Props) {
     const badgeElements = badges && badges.map(text => (
-        <span key={text} className="badge text-bg-secondary me-1">{text}</span>
+        <span key={text} className='badge text-bg-secondary me-1'>{text}</span>
     ))
 
     return (
-        <section className="container px-4 pb-4">
-            <div className="card">
-                <div className="row g-0">
+        <section className='container px-4 pb-4'>
+            <div className='card'>
+                <div className='row g-0'>
 
-                    <div className="col-md-8">
-                        <div className="card-body m-4">
-                            <h5 className="card-title">{title}</h5>
+                    <div className='col-md-8'>
+                        <div className='card-body m-4'>
+                            <h5 className='card-title'>{title}</h5>
 
-                            <div className="my-2">{badgeElements}</div>
+                            <div className='my-2'>{badgeElements}</div>
 
-                            <p className="card-text">{description}</p>
+                            <p className='card-text'>{description}</p>
 
-                            {demoUrl && <a className="btn btn-primary me-2" href={demoUrl} role="button">Demo</a>}
-                            {sourceUrl && <a className="btn btn-secondary" href={sourceUrl} role="button">Source Code</a>}
+                            {demoUrl && <a className='btn btn-primary me-2' href={demoUrl} role='button'>Demo</a>}
+                            {sourceUrl && <a className='btn btn-secondary' href={sourceUrl} role='button'>Source Code</a>}
                         </div>
                     </div>
 
