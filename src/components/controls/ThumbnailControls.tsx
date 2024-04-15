2bed64fec773e8bb63f8e92793233dfda3239311
wizguin <16276645+wizguin@users.noreply.github.com>
2024-04-15 13:44:03 +0100

    Update ThumbnailControls to use thumbnail file

--- src/components/controls/ThumbnailControls.tsx
+++ src/components/controls/ThumbnailControls.tsx
@@ -1,7 +1,5 @@
 import './ThumbnailControls.css'
 
-import { PreviewTypes, getType } from '../types/PreviewTypes'
-
 import { Dispatch, SetStateAction, SyntheticEvent } from 'react'
 
 interface Props {
@@ -12,44 +10,22 @@ interface Props {
 
 export default function ThumbnailControls({ media, mediaIndex, setMediaIndex }: Props) {
     const thumbnails = media.map((fileName, index) => {
-        const type = getType(fileName)
-        const src = `assets/previews/${fileName}`
-
         const className = `thumbnail ${index === mediaIndex && 'selected'}`
+
+        const src = `assets/thumbnails/${fileName.split('.')[0]}.jpg`
 
         const onClick = (event: SyntheticEvent) => {
             event.stopPropagation()
+
             setMediaIndex(index)
         }
 
-        let thumbnail: JSX.Element
-
-        switch (type) {
-            case PreviewTypes.Image:
-                thumbnail = <img
-                    className={className}
-                    src={src}
-                    role='button'
-                    onClick={onClick}
-                />
-                break
-
-            case PreviewTypes.Video:
-                thumbnail = <video
-                    className={className}
-                    preload='metadata'
-                    src={src}
-                    role='button'
-                    onClick={onClick}
-                />
-                break
-
-            default:
-                thumbnail =  <></>
-                break
-        }
-
-        return thumbnail
+        return <img
+            className={className}
+            src={src}
+            role='button'
+            onClick={onClick}
+        />
     })
 
     return (
