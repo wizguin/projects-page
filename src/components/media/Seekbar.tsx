26deda821044f96eec97edef62765b97faa98328
wizguin <16276645+wizguin@users.noreply.github.com>
2024-04-17 21:46:51 +0100

    Add drag seeking video

--- src/components/media/Seekbar.tsx
+++ src/components/media/Seekbar.tsx
@@ -1,6 +1,7 @@
 import './Seekbar.css'
 
-import { useState, useEffect, useRef, MouseEvent, MutableRefObject } from 'react'
+import { useState, useEffect, useRef, useCallback,
+    MutableRefObject, MouseEvent as ReactMouseEvent } from 'react'
 
 interface Props {
     videoRef: MutableRefObject<HTMLVideoElement | null>,
@@ -9,10 +10,23 @@ interface Props {
 
 export default function Seekbar({ videoRef, visible }: Props) {
     const [progress, setProgress] = useState(0)
+    const [isSeeking, setIsSeeking] = useState(false)
 
     const seekbarRef = useRef<HTMLDivElement | null>(null)
 
     const hideSeekbar = visible ? '' : 'hidden'
+
+    const seek = useCallback((clientX: number) => {
+        if (!seekbarRef.current) return
+
+        const rect = seekbarRef.current.getBoundingClientRect()
+        const x = clientX - rect.left
+        const percent = x / rect.width
+
+        if (videoRef.current) {
+            videoRef.current.currentTime = videoRef.current.duration * percent
+        }
+    }, [videoRef])
 
     useEffect(() => {
         function updateProgress() {
@@ -28,18 +42,42 @@ export default function Seekbar({ videoRef, visible }: Props) {
         }
     }, [videoRef])
 
-    function onSeekbarClick(event: MouseEvent) {
-        event.stopPropagation()
-
-        if (!seekbarRef.current) return
-
-        const rect = seekbarRef.current.getBoundingClientRect()
-        const x = event.clientX - rect.left
-        const percent = x / rect.width
-
-        if (videoRef.current) {
-            videoRef.current.currentTime = videoRef.current.duration * percent
+    useEffect(() => {
+        function addEvents() {
+            window.addEventListener('mousemove', onMouseMove)
+            window.addEventListener('mouseup', onMouseUp)
         }
+
+        function removeEvents() {
+            window.removeEventListener('mousemove', onMouseMove)
+            window.removeEventListener('mouseup', onMouseUp)
+        }
+
+        function onMouseMove(event: MouseEvent) {
+            seek(event.clientX)
+        }
+
+        function onMouseUp()  {
+            setIsSeeking(false)
+        }
+
+        if (isSeeking) {
+            addEvents()
+        } else {
+            removeEvents()
+        }
+
+        return removeEvents
+    }, [isSeeking, seek])
+
+    function onSeekbarClick(event: ReactMouseEvent) {
+        seek(event.clientX)
+    }
+
+    function onSeekbarDown(event: ReactMouseEvent) {
+        event.preventDefault()
+
+        setIsSeeking(true)
     }
 
     return (
@@ -47,6 +85,7 @@ export default function Seekbar({ videoRef, visible }: Props) {
             className={`seekbar-container fade ${hideSeekbar}`}
             role='button'
             onClick={onSeekbarClick}
+            onMouseDown={onSeekbarDown}
         >
             <div className='seekbar-bg' ref={seekbarRef}>
                 <div className='seekbar' style={{ width: `${progress * 100}%` }}></div>
