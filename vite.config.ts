fa96d7dedebe7c465e0f6eb3bc7c65b4980a7316
wizguin <16276645+wizguin@users.noreply.github.com>
2024-04-25 17:12:04 +0100

    Update build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+        react(),
+        createHtmlPlugin({
+            minify: true,
+            entry: 'src/main.tsx',
+            inject: {
+                data: {
+                    title: 'Portfolio'
+                }
+            }
+        })
+    ]
 })
