1130fcfe02d744500b943f23cbc451ae8cf35c25
wizguin <16276645+wizguin@users.noreply.github.com>
2024-06-12 10:22:28 +0100

    Add dropdown button component

---
+++ src/components/section/card/button/DropdownButton.tsx
@@ -0,0 +1,80 @@
+import './DropdownButton.css'
+
+import { classNames } from '../../../../utils/Utils'
+
+import { useState, useEffect, useRef } from 'react'
+
+type Option = { label: string, url: string }
+
+export interface Props {
+    label: string,
+    options: Option[]
+}
+
+export default function DropdownButton({ label, options }: Props) {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const buttonRef = useRef<HTMLAnchorElement | null>(null)
+
+    const dropdownMenu = (
+        <div className={classNames('dropdown-menu fade-in', !isOpen && 'display-none')}>
+            {options.map(option => dropdownItem(option))}
+        </div>
+    )
+
+    function dropdownItem(option: Option) {
+        return (
+            <a
+                className='button'
+                href={option.url}
+                target='_blank'
+                role='button'
+            >
+                {option.label}
+            </a>
+        )
+    }
+
+    // Mouse events
+    useEffect(() => {
+        function addEvents() {
+            window.addEventListener('mouseup', onMouseUp)
+        }
+
+        function removeEvents() {
+            window.removeEventListener('mouseup', onMouseUp)
+        }
+
+        function onMouseUp(event: MouseEvent) {
+            // Close if mouse event was outside toggle button
+            if (event.button === 0
+                && buttonRef.current
+                && !buttonRef.current.contains(event.target as Node)) {
+                setIsOpen(false)
+            }
+        }
+
+        if (isOpen) {
+            addEvents()
+        } else {
+            removeEvents()
+        }
+
+        return removeEvents
+    }, [isOpen])
+
+    return (
+        <div>
+            <a
+                ref={buttonRef}
+                className='button button-secondary'
+                role='button'
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                {label}
+            </a>
+
+            {dropdownMenu}
+        </div>
+    )
+}
