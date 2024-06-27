481cc2bba303cc3e6a356f72cd3cd69d4ec0ce94
wizguin <16276645+wizguin@users.noreply.github.com>
2024-06-27 10:04:15 +0100

    Add notificiation type classnames

--- src/components/notifications/notification/Notification.tsx
+++ src/components/notifications/notification/Notification.tsx
@@ -1,14 +1,25 @@
 import './Notification.css'
 
-import { Notification as Props } from '../../../notifications/NotificationContext'
+import { Notification as Props, NotificationType } from '../../../notifications/NotificationContext'
 import { useNotifications } from '../../../notifications/NotificationContext'
 
 export default function Notification({ id, message, type }: Props)  {
     const { remove } = useNotifications()
 
+    let style: string
+
+    switch (type) {
+        case NotificationType.Success:
+            style = 'success'
+            break
+        case NotificationType.Error:
+            style = 'error'
+            break
+    }
+
     return (
         <div
-            className='notification fade-in'
+            className={`notification fade-in ${style}`}
             role='button'
             onClick={() => remove(id)}
         >
--- src/notifications/NotificationContext.tsx
+++ src/notifications/NotificationContext.tsx
@@ -10,7 +10,7 @@ export interface Notification {
     type: NotificationType
 }
 
-enum NotificationType {
+export enum NotificationType {
     Success,
     Error
 }
